403d29d765031384eab9476f75161d4b77da3bf5
un300 <lan4148@naver.com>
2021-01-23 21:32:35 +0900

    Algorithm

--- 01_이것이 취업을 위한 코딩테스트이다/chapter07_이진탐색/05_[실전문제]_부품찾기/02_부품찾기_나의코드.py
+++ 01_이것이 취업을 위한 코딩테스트이다/chapter07_이진탐색/05_[실전문제]_부품찾기/02_부품찾기_나의코드.py
@@ -1,1 +1,28 @@
-N = int(input)
+# 이진탐색 구현
+def binary_search(array, target):
+    start = 0
+    end   = len(array)
+
+    while start < end :
+        mid = (start + end) // 2
+        if array[mid] == target :
+            return True
+        elif array[mid] > target:
+            end = mid-1
+        else :
+            start = mid + 1
+
+    return False
+
+# 입력
+N = int(input())
+product_number_list = list(map(int, input().split()))
+M = int(input())
+find_number_list = list(map(int, input().split()))
+
+# 이진탐색을 적용하기 위해 정렬
+sorted_product_number_list = sorted(product_number_list)
+
+# 출력
+answer = ['yes' if binary_search(sorted_product_number_list, element) else 'no' for element in find_number_list]
+print(answer)
