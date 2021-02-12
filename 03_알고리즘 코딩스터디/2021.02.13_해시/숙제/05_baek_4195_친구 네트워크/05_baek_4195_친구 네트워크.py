f943a541f80bef34af6e5c2c104edf2709932fdb
un300 <lan4148@naver.com>
2021-02-12 19:02:03 +0900

    Union Find

--- 03_알고리즘 코딩스터디/2021.02.13_해시/숙제/05_baek_4195_친구 네트워크/05_baek_4195_친구 네트워크.py
+++ 03_알고리즘 코딩스터디/2021.02.13_해시/숙제/05_baek_4195_친구 네트워크/05_baek_4195_친구 네트워크.py
@@ -22,31 +22,41 @@ class FriendNetwork :
     def getUnion(self, friend01, friend02) :
         friend01_idx = self.index_dict[friend01]
         friend02_idx = self.index_dict[friend02]
+
         friend01_parent = self.getParent(friend01_idx)
         friend02_parent = self.getParent(friend02_idx)
 
         if friend01_parent < friend02_parent :
             self.arr[friend02_idx] = friend01_parent
+            while friend02_parent in self.arr  :
+                index = self.arr.index(friend02_parent)
+                self.arr[index] = friend01_parent
         else :
             self.arr[friend01_idx] = frined02_parent
+            while friend01_parent in self.arr :
+                index = self.arr.index(friend01_idx)
+                self.arr[index] = friend02_parent
 
+    def print_network(self) :
+        print(self.arr.count(0))
+
+import sys
 
 
 def solution() :
-    T = int(input())
+    fast_input = sys.stdin.readline
+    T = int(fast_input().rstrip())
     for _ in range(T) :
         instance = FriendNetwork()
-        F = int(input())
+        F = int(fast_input().rstrip())
         for _ in range(F) :
-            F1, F2 = list(input().split())
+            F1, F2 = list(fast_input().rstrip().split())
             instance.getArr(F1)
             instance.getArr(F2)
             
             instance.getUnion(F1, F2)
-
-            print(instance.arr)
-            print(instance.index_dict)
-
+            instance.print_network()
+        
 
 solution()
 
