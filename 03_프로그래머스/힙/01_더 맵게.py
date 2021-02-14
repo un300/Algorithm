bae18f7977a5574ab08b5ae959cca1d2a8ee3b9c
un300 <lan4148@naver.com>
2021-02-14 18:05:01 +0900

    Algorithm

--- 03_프로그래머스/힙/01_더 맵게.py
+++ 03_프로그래머스/힙/01_더 맵게.py
@@ -4,19 +4,18 @@ def solution(scoville, K):
     heapq.heapify(scoville)
     cnt = 0
     
-    while scoville[0] < K :
+    while (scoville[0] < K):
         min01 = heapq.heappop(scoville)
         min02 = heapq.heappop(scoville)
         new = min01 + (min02 * 2)
         
-        if new == 0:
-            return -1
-        
         heapq.heappush(scoville, new)
         cnt += 1
         
-        if len(scoville) < 2:
-            break
+        if (not scoville):
+            return -1
+        elif (len(scoville) == 1) & (scoville[0] < K):
+            return -1
     return cnt
 
 
